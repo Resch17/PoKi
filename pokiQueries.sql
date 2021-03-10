fed94543d827b102e1ce3e1ac587f0e4ecd715d3
Aaron Resch <aaronmresch@gmail.com>
2021-03-10 12:16:05 -0600

    saving progress

---
+++ pokiQueries.sql
@@ -0,0 +1,35 @@
+﻿-- 1
+/*
+SELECT Name
+	FROM Grade;
+-- 1st thru 5th grade
+*/
+
+--2 
+/*
+SELECT Name
+	FROM Emotion;
+-- Anger, Fear, Sadness, Joy
+*/
+
+--3
+/*
+SELECT Count(*) as PoemCount
+	FROM Poem;
+-- 32842
+*/
+
+--4
+/*
+SELECT Top 76 Name
+	FROM Author
+	ORDER BY Name;
+*/
+
+--5
+/*
+SELECT Top 76 a.Name, g.Name as Grade
+	FROM Author a
+		LEFT JOIN Grade g on g.Id = a.GradeId
+	ORDER BY a.Name;
+*/
